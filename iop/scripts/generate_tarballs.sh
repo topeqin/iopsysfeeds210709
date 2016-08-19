add35c243fa839c7af6e117d868da22a83debb0b
Sukru Senli <sukru.senli@inteno.se>
2016-08-19 17:25:02 +0200

    iop: improve generate_tarballs script

--- iop/scripts/generate_tarballs.sh
+++ iop/scripts/generate_tarballs.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+SERVER="god@software.inteno.se"
+PATH="/var/www/html/iopsys/consumer"
+
 build_bcmkernel_consumer() {
 	local tarfile bcmkernelcommith
 	bcmkernelcommith=$(grep -w "PKG_SOURCE_VERSION:" $curdir/feeds/feed_inteno_broadcom/bcmkernel/$sdkversion.mk | cut -d'=' -f2)
 	# do not build bcmopen sdk if it was already built before
-	ssh god@software.inteno.se "ls /var/www/html/iopsys/consumer/bcmopen-$profile-$bcmkernelcommith.tar.gz" && return
+	ssh $SERVER "ls $PATH/bcmopen-$profile-$bcmkernelcommith.tar.gz" && return
 	cd ./build_dir/target-*_uClibc-0.9.33.*/bcmkernel-3.4-$sdkversion/bcm963xx/release
 	sh do_consumer_release -p $profile -y
 	tarfile='out/bcm963xx_*_consumer.tar.gz'
 	[ $(ls -1 $tarfile |wc -l) -ne 1 ] && echo "Too many tar files: '$tarfile'" && return
-	scp $tarfile god@software.inteno.se:/var/www/html/iopsys/consumer/bcmopen-$profile-$bcmkernelcommith.tar.gz
+	scp $tarfile $SERVER:$PATH/bcmopen-$profile-$bcmkernelcommith.tar.gz
 	rm -f $tarfile
 	cd $curdir
 }
@@ -20,12 +23,12 @@ build_natalie_consumer() {
 	grep -q "CONFIG_TARGET_NO_DECT=y" .config && return
 	natalieversion=$(grep -w "PKG_VERSION:" ./feeds/feed_inteno_packages/natalie-dect/Makefile | cut -d'=' -f2)
 	nataliecommith=$(grep -w "PKG_SOURCE_VERSION:" ./feeds/feed_inteno_packages/natalie-dect/Makefile | cut -d'=' -f2)
-	ssh god@software.inteno.se "ls public/www/iopsys/consumer/natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz" && return
+	ssh $SERVER "ls $PATH/natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz" && return
 	cd ./build_dir/target-*_uClibc-0.9.33.*/natalie-dect-$natalieversion/
 	mkdir natalie-dect-open-$natalieversion
 	cp NatalieFpCvm6362/Src/Projects/NatalieV3/FpCvm/Linux6362/dects.ko natalie-dect-open-$natalieversion/dect.ko
 	tar -czv  natalie-dect-open-$natalieversion/ -f natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz
-	scp natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz god@software.inteno.se:/var/www/html/iopsys/consumer/
+	scp natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz $SERVER:$PATH/
 	cp natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz $curdir/
 	rm -rf natalie-dect-open-$natalieversion
 	rm -f natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz
@@ -38,12 +41,12 @@ build_endptcfg_consumer() {
 	grep -q "CONFIG_TARGET_NO_VOICE=y" .config && return
 	endptversion=$(grep -w "PKG_VERSION:" ./feeds/feed_inteno_packages/endptcfg/Makefile | cut -d'=' -f2)
 	endptcommith=$(grep -w "PKG_SOURCE_VERSION:" ./feeds/feed_inteno_packages/endptcfg/Makefile | cut -d'=' -f2)
-	ssh god@software.inteno.se "ls public/www/iopsys/consumer/endptcfg-$profile-$endptversion-$endptcommith.tar.gz" && return
+	ssh $SERVER "ls $PATH/endptcfg-$profile-$endptversion-$endptcommith.tar.gz" && return
 	cd ./build_dir/target-*_uClibc-0.9.33.*/endptcfg-$endptversion/
 	mkdir endptcfg-open-$endptversion
 	cp endptcfg endptcfg-open-$endptversion/
 	tar -czv  endptcfg-open-$endptversion/ -f endptcfg-$profile-$endptversion-$endptcommith.tar.gz
-	scp endptcfg-$profile-$endptversion-$endptcommith.tar.gz god@software.inteno.se:/var/www/html/iopsys/consumer/
+	scp endptcfg-$profile-$endptversion-$endptcommith.tar.gz $SERVER:$PATH/
 	cp endptcfg-$profile-$endptversion-$endptcommith.tar.gz $curdir/
 	rm -rf endptcfg-open-$endptversion
 	rm -f endptcfg-$profile-$endptversion-$endptcommith.tar.gz
@@ -57,10 +60,10 @@ build_ice_consumer() {
 	icebasever=$(grep -w "BASE_PKG_VERSION:" ./feeds/feed_inteno_packages/ice-client/Makefile | cut -d'=' -f2)
 	icerelease=$(grep -w "PKG_RELEASE:" ./feeds/feed_inteno_packages/ice-client/Makefile | cut -d'=' -f2)
 	iceversion=$icebasever$icerelease
-	ssh god@software.inteno.se "ls public/www/iopsys/consumer/ice-client-$profile-$iceversion-$icecommith.tar.gz" && return
+	ssh $SERVER "ls $PATH/ice-client-$profile-$iceversion-$icecommith.tar.gz" && return
 	cd ./build_dir/target-*_uClibc-0.9.33.*/ice-client-$icebasever/ipkg-*
 	tar -czv  ice-client -f ice-client-$profile-$iceversion-$icecommith.tar.gz
-	scp ice-client-$profile-$iceversion-$icecommith.tar.gz god@software.inteno.se:/var/www/html/iopsys/consumer/
+	scp ice-client-$profile-$iceversion-$icecommith.tar.gz $SERVER:$PATH/
 	cp ice-client-$profile-$iceversion-$icecommith.tar.gz $curdir/
 	rm -f ice-client-$profile-$iceversion-$icecommith.tar.gz
 	cd $curdir
