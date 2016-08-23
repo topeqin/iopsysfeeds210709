2c5eb37f4688230420c959d19d10a4b71967059b
Sukru Senli <sukru.senli@inteno.se>
2016-08-23 10:00:50 +0200

    iop: change PATH to FPATH in generate_tarballs script

--- iop/scripts/generate_tarballs.sh
+++ iop/scripts/generate_tarballs.sh
@@ -5,12 +5,12 @@ build_bcmkernel_consumer() {
 	local tarfile bcmkernelcommith
 	bcmkernelcommith=$(grep -w "PKG_SOURCE_VERSION:" $curdir/feeds/feed_inteno_broadcom/bcmkernel/$sdkversion.mk | cut -d'=' -f2)
 	# do not build bcmopen sdk if it was already built before
-	ssh $SERVER "ls $PATH/bcmopen-$profile-$bcmkernelcommith.tar.gz" && return
+	ssh $SERVER "ls $FPATH/bcmopen-$profile-$bcmkernelcommith.tar.gz" && return
 	cd ./build_dir/target-*_uClibc-0.9.33.*/bcmkernel-3.4-$sdkversion/bcm963xx/release
 	sh do_consumer_release -p $profile -y
 	tarfile='out/bcm963xx_*_consumer.tar.gz'
 	[ $(ls -1 $tarfile |wc -l) -ne 1 ] && echo "Too many tar files: '$tarfile'" && return
-	scp $tarfile $SERVER:$PATH/bcmopen-$profile-$bcmkernelcommith.tar.gz
+	scp $tarfile $SERVER:$FPATH/bcmopen-$profile-$bcmkernelcommith.tar.gz
 	rm -f $tarfile
 	cd $curdir
 }
@@ -21,12 +21,12 @@ build_natalie_consumer() {
 	grep -q "CONFIG_TARGET_NO_DECT=y" .config && return
 	natalieversion=$(grep -w "PKG_VERSION:" ./feeds/feed_inteno_packages/natalie-dect/Makefile | cut -d'=' -f2)
 	nataliecommith=$(grep -w "PKG_SOURCE_VERSION:" ./feeds/feed_inteno_packages/natalie-dect/Makefile | cut -d'=' -f2)
-	ssh $SERVER "ls $PATH/natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz" && return
+	ssh $SERVER "ls $FPATH/natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz" && return
 	cd ./build_dir/target-*_uClibc-0.9.33.*/natalie-dect-$natalieversion/
 	mkdir natalie-dect-open-$natalieversion
 	cp NatalieFpCvm6362/Src/Projects/NatalieV3/FpCvm/Linux6362/dects.ko natalie-dect-open-$natalieversion/dect.ko
 	tar -czv  natalie-dect-open-$natalieversion/ -f natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz
-	scp natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz $SERVER:$PATH/
+	scp natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz $SERVER:$FPATH/
 	cp natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz $curdir/
 	rm -rf natalie-dect-open-$natalieversion
 	rm -f natalie-dect-$profile-$natalieversion-$nataliecommith.tar.gz
@@ -39,12 +39,12 @@ build_endptcfg_consumer() {
 	grep -q "CONFIG_TARGET_NO_VOICE=y" .config && return
 	endptversion=$(grep -w "PKG_VERSION:" ./feeds/feed_inteno_packages/endptcfg/Makefile | cut -d'=' -f2)
 	endptcommith=$(grep -w "PKG_SOURCE_VERSION:" ./feeds/feed_inteno_packages/endptcfg/Makefile | cut -d'=' -f2)
-	ssh $SERVER "ls $PATH/endptcfg-$profile-$endptversion-$endptcommith.tar.gz" && return
+	ssh $SERVER "ls $FPATH/endptcfg-$profile-$endptversion-$endptcommith.tar.gz" && return
 	cd ./build_dir/target-*_uClibc-0.9.33.*/endptcfg-$endptversion/
 	mkdir endptcfg-open-$endptversion
 	cp endptcfg endptcfg-open-$endptversion/
 	tar -czv  endptcfg-open-$endptversion/ -f endptcfg-$profile-$endptversion-$endptcommith.tar.gz
-	scp endptcfg-$profile-$endptversion-$endptcommith.tar.gz $SERVER:$PATH/
+	scp endptcfg-$profile-$endptversion-$endptcommith.tar.gz $SERVER:$FPATH/
 	cp endptcfg-$profile-$endptversion-$endptcommith.tar.gz $curdir/
 	rm -rf endptcfg-open-$endptversion
 	rm -f endptcfg-$profile-$endptversion-$endptcommith.tar.gz
@@ -58,10 +58,10 @@ build_ice_consumer() {
 	icebasever=$(grep -w "BASE_PKG_VERSION:" ./feeds/feed_inteno_packages/ice-client/Makefile | cut -d'=' -f2)
 	icerelease=$(grep -w "PKG_RELEASE:" ./feeds/feed_inteno_packages/ice-client/Makefile | cut -d'=' -f2)
 	iceversion=$icebasever$icerelease
-	ssh $SERVER "ls $PATH/ice-client-$profile-$iceversion-$icecommith.tar.gz" && return
+	ssh $SERVER "ls $FPATH/ice-client-$profile-$iceversion-$icecommith.tar.gz" && return
 	cd ./build_dir/target-*_uClibc-0.9.33.*/ice-client-$icebasever/ipkg-*
 	tar -czv  ice-client -f ice-client-$profile-$iceversion-$icecommith.tar.gz
-	scp ice-client-$profile-$iceversion-$icecommith.tar.gz $SERVER:$PATH/
+	scp ice-client-$profile-$iceversion-$icecommith.tar.gz $SERVER:$FPATH/
 	cp ice-client-$profile-$iceversion-$icecommith.tar.gz $curdir/
 	rm -f ice-client-$profile-$iceversion-$icecommith.tar.gz
 	cd $curdir
@@ -70,7 +70,7 @@ build_ice_consumer() {
 function generate_tarballs {
 
     SERVER="god@software.inteno.se"
-    PATH="/var/www/html/iopsys/consumer"
+    FPATH="/var/www/html/iopsys/consumer"
 
     git remote -v | grep -q http && return # do not continue if this is an open SDK environment
 
