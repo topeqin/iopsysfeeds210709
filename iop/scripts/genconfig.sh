26f704b4d2a822bf6e6a7e45a71ba3ca68347809
Jonas Höglund <jonas.hoglund@embeddednation.com>
2017-08-25 09:51:16 +0200

    Add dg400primeb board

--- iop/scripts/genconfig.sh
+++ iop/scripts/genconfig.sh
@@ -15,7 +15,7 @@ function genconfig {
 	LOCAL_MIRROR="http://mirror.inteno.se/mirror"
 
 	iopsys_brcm63xx_mips="cg300 cg301 dg150 dg150v2 dg150alv2 dg200 dg200al dg301 dg301al eg300 vg50 vox25"
-	iopsys_brcm63xx_arm="dg400 eg400 f104w f104 dg400prime"
+	iopsys_brcm63xx_arm="dg400 eg400 f104w f104 dg400prime dg400primeb"
 	iopsys_ramips="ex400"
 	ramips="mt7621"
 	target="bogus"
