89afdc3012341630ef3984f9ab56ccde0169c0e7
Kenneth Johansson <kenneth@southpole.se>
2018-10-01 19:46:35 +0200

    rename target iopsys-grx to intel_mips.

--- iop/scripts/feeds_update.sh
+++ iop/scripts/feeds_update.sh
@@ -35,7 +35,7 @@ function feeds_update {
     ./scripts/feeds install -p targets iopsys-brcm63xx-mips
     ./scripts/feeds install -p targets iopsys-brcm63xx-arm
     ./scripts/feeds install -p targets iopsys-ramips
-    ./scripts/feeds install -p targets_grx iopsys-grx
+    ./scripts/feeds install -p intel_targets intel_mips
 
     # install all packages
     ./scripts/feeds install -a 
--- iop/scripts/genconfig.sh
+++ iop/scripts/genconfig.sh
@@ -24,7 +24,7 @@ function genconfig {
 	    local iopsys_brcm63xx_mips="cg300 cg301 dg150 dg150v2 dg150alv2 dg200 dg200al dg301 dg301al eg300 vg50 vox25 hpc_hu02 eg200"
 	    local iopsys_brcm63xx_arm="dg400 eg400 sdx810_rg f104w f104 dg400prime dg400primeb 963138REF_P502"
 	    local iopsys_ramips="ex400 sdx810_ap"
-	    local iopsys_grx="grx500"
+	    local intel_mips="easy550"
 	    local ramips="mt7621"
 
 
@@ -59,9 +59,9 @@ function genconfig {
 		fi
 	    done
 
-	    for p in $iopsys_grx; do
+	    for p in $intel_mips; do
 		if [ $p == $profile ]; then
-		    target="iopsys_grx"
+		    target="intel_mips"
 		    return
 		fi
 	    done
