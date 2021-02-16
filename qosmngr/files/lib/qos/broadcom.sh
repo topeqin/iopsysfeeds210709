a8e48ca42877928d7e057b870ec172a8710e2642
Rahul <rahul.thakur@iopsys.eu>
2021-02-16 13:29:06 +0530

    qosmngr: fix 4323 on devel
    Although no devel panther or FT is not supported as yet but that
    might be the case in near future so keeping it same here plus,
    bs alias should be replaced with proper bdmf_shell command anyway
    so this is done here as well. No regression, no harm.

--- qosmngr/files/lib/qos/broadcom.sh
+++ qosmngr/files/lib/qos/broadcom.sh
@@ -646,17 +646,29 @@ assign_policer_to_port() {
 	local portorder="$(db -q get hw.board.ethernetPortOrder)"
 	local wanport="$(db -q get hw.board.ethernetWanPort)"
 
+	local board="$(db -q get hw.board.model_name | cut -c 1-2)"
+
 	local i=0
+	if [ "$board" == "FT" ]; then
+		i=3
+	fi
+
 	for port in $portorder; do
 		if [ "$ifname" == "$port" ]; then
 			if [ "$wanport" == "$port" ]; then
-				bs /b/configure port/index=wan0 ingress_rate_limit={traffic_types=8,policer={policer/dir=us,index=$pindex}}
+				bdmf_shell -c `cat /var/bdmf_sh_id` -cmd /b/configure port/index=wan0 ingress_rate_limit={traffic_types=8,policer={policer/dir=us,index=$pindex}}
 			else
-				bs /b/configure port/index=lan$i ingress_rate_limit={traffic_types=8,policer={policer/dir=us,index=$pindex}}
+				bdmf_shell -c `cat /var/bdmf_sh_id` -cmd /b/configure port/index=lan$i ingress_rate_limit={traffic_types=8,policer={policer/dir=us,index=$pindex}}
 			fi
 			break
 		fi
-		i=$((i + 1))
+
+		if [ "$board" == "FT" ]; then
+			i=$((i - 1))
+		else
+			i=$((i + 1))
+		fi
+
 	done
 }
 
